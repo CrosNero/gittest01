1d415fb599d22ba1c76553f4b39799cde6ac5327
xsj <465118615@qq.com>
2022-07-26 17:06:41 +0800

    second commit

---
+++ src/main/java/com/xsj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xsj.git;
+
+/**
+ * @author by xsj
+ * @date 2022-07-26 17:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git_01");
+    }
+}
